e240dc932a7f339146b46925c05699ea1296ab61
KienPhan1109 <phantrungkien2006@gmail.com>
2025-02-11 14:35:24 +0700

    Update function.h

--- function.h
+++ function.h
@@ -4,7 +4,7 @@
 #include "datatype.h"
 
 // Ham xoa bo nho dem
-void clearInput();
+void clearCache();
 
 // Ham nhap lua chon
 int choice();
@@ -12,11 +12,17 @@ int choice();
 // Ten he thong
 void nameSystem();
 
-// Ham doc FIle
+// Ham doc FIle categories.bin
 void readFile(Category categories[], int *count);
 
-// Ham ghi File
+// Ham ghi File categories.bin
 void writeFile(const Category categories[], int count);
+
+// Ham doc File admin.bin
+void readFileAdmin(Admin admin[], int *countAdmin);
+
+// Ham ghi File admin.bin
+void writeFileAdmin(const Admin admin[], int countAmin);
 
 // Ham hien thi toan bo danh muc
 void displayCategories(const Category categories[], int count);
@@ -39,7 +45,34 @@ void sortCategoriesUp(Category categories[], int count);
 // Ham sap xep danh muc theo ten giam dan
 void sortCategoriesDown(Category categories[], int count);
 
-// Ham thoat chuong trinh
-void exitSystem();
+// Ten ham them danh muc
+void nameAddCategory();
+
+// Ham ten sua danh muc
+void nameEditCategory();
+
+// Ham ten xoa danh muc
+void nameDeleteCategory();
+
+// Ham ten hien thi danh muc
+void nameShowCategory();
+
+// Ham kiem tra trung lap ID
+int isDuplicateID(Category categories[], int count, char id[]);
+
+// Ham kiem tra trung lap Name
+int isDuplicateName(Category categories[], int count, char name[]);
+
+// Ham quay lai menu
+void goBack(Category categories[], int *count);
+
+// Ham lua chon cach sap xep
+void sortCategoriesMenu(Category categories[], int count);
+
+// Ham lua chon cac chuc nang danh muc
+void mainMenu(Category categories[], int *count);
+
+// Ham dang nhap
+void LoginAdmin(Admin admin[], int countAmin);
 
 #endif
