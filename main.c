6c230e47be9b530512f9b576a27a3e80a811eb30
KienPhan1109 <phantrungkien2006@gmail.com>
2025-02-11 14:35:35 +0700

    Update main.c

--- main.c
+++ main.c
@@ -5,70 +5,23 @@
 
 int main() {
     Category categories[100];
-    int count;
-    int c;
+
+    // Tai khoan Admin
+    Admin admin[100] = {
+        {"KienPhan", "11092006"}
+    };
+
+    int countAdmin = 1;
+    int count = 0;
+
+    writeFileAdmin(admin, countAdmin);
+
+    LoginAdmin(admin, countAdmin);
 
     readFile(categories, &count);
 
-    while (1)
-    {
-        nameSystem();
-        c = menuCategory();
-        switch (c) 
-        {
-            case 1: // Hien thi toan bo danh muc
-                displayCategories(categories, count);
-                break;
+    mainMenu(categories, &count);
 
-            case 2: // Them danh muc
-                addCategory(categories, &count);
-                break;
-
-            case 3: // Sua danh muc
-                editCategory(categories, count);
-                break;
-
-            case 4: // Xoa danh muc
-                deleteCategory(categories, &count);
-                break;
-
-            case 5: // Tim kiem danh muc theo ten
-                searchCategory(categories, count);
-                break;
-
-            case 6: // Sap xep danh muc theo ten
-                {
-                    printf("Nhap vao lua chon sap xep.\n");
-                    printf("1 : Tang dan, 2 : Giam dan\n");
-                    int c = choice();
-                    switch (c)
-                    {
-                        case 1: // Sap xep danh muc theo ten tang dan
-                            sortCategoriesUp(categories, count);
-                            break;
-                        
-                        case 2: // Sap xep danh muc theo ten giam dan
-                            sortCategoriesDown(categories, count);
-                            break;
-
-                        default:
-                            printf("Lua chon khong hop le.\n");
-                            break;
-                    }
-                    break;
-                }
-                
-            case 0: // Thoat chuong trinh
-                exitSystem();
-                system("pause");
-                return 0;
-                break;
-
-            default:
-                printf("Invalid option. Try again.\n");
-                break;
-        }
-    }
     system("pause");
     return 0;
 }
