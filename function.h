3a7d0553db107e4511a1fe518006e855dfb13fb5
KienPhan1109 <phantrungkien2006@gmail.com>
2025-02-15 22:59:14 +0700

    Add files via upload

---
+++ function.h
@@ -0,0 +1,73 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "datatype.h"
+
+
+void writeFileAdmin(const Admin admin[], int countAdmin);
+
+void readFileAdmin(Admin admin[], int *countAdmin);
+
+void LoginAdmin(Admin admin[], int countAdmin);
+
+void mainMenu();
+
+int choice();
+
+void clearCache();
+
+void goBack();
+
+int isDuplicateIDCategory(Category categories[], int countCategory, char id[]);
+
+int isDuplicateNameCategory(Category categories[], int countCategory, char name[]);
+
+int isDuplicateIDProductInCategory(Product products[], int countProduct, char *productID, char *categoryID);
+
+int isDuplicateNameProductInCategory(Product products[], int countProduct, char *productName, char *categoryID);
+
+void writeFileCategory(const Category categories[], int countCategory);
+
+void writeFileProduct(const Product products[], int countProduct);
+
+void readFileCategory(Category categories[], int *countCategory);
+
+void readFileProduct(Product products[], int *countProduct);
+
+void displayCategories(const Category categories[], int countCategory);
+
+void displayProducts(const Product products[], int countProduct);
+
+void addCategory(Category categories[], int *countCategory);
+
+void addProduct(Product products[], int *countProduct, Category categories[], int *countCategory);
+
+void editCategory(Category categories[], int countCategory);
+
+void editProduct(Product products[], int *countProduct, Category categories[], int *countCategory);
+
+void deleteCategory(Category categories[], int *countCategory, Product products[], int *countProduct);
+
+void deleteProduct(Product products[], int *countProduct, Category categories[], int *countCategory);
+
+void searchCategory(const Category categories[], int countCategory);
+
+void searchProduct(const Product products[], int countProduct);
+
+void sortCategoriesUp(Category categories[], int countCategory);
+
+void sortCategoriesDown(Category categories[], int countCategory);
+
+void sortProductsUp(Product products[], int countProduct);
+
+void sortProductsDown(Product products[], int countProduct);
+
+void menuCategory(Category categories[], int *count, Product products[], int *countProduct);
+
+void menuProduct(Product products[], int *countProduct, Category categories[] , int *countCategory);
+
+void sortCategoriesMenu(Category categories[], int count);
+
+void sortProductsMenu(Product products[], int countProduct);
+
+#endif
