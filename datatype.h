c5b73bbec77ef06c477c3d3927a2ef1cad2b817c
KienPhan1109 <phantrungkien2006@gmail.com>
2025-02-15 14:34:35 +0700

    Update datatype.h

--- datatype.h
+++ datatype.h
@@ -7,10 +7,20 @@ typedef struct {
     char CategoryName[10];
 } Category;
 
+// Dinh nghia struct Login
 typedef struct 
 {
     char Account[30];
     char Password[30];
 } Admin;
+
+// Dinh nghia struct Product
+typedef struct {
+    char ProductId[10];
+    char CategoryId[10];
+    char ProductName[10];
+    int Quantity;
+    int Price;
+} Product;
 
 #endif
